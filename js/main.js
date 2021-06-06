17f5d9fe233a0759f7b768c4a99164351b6fb984
Mary Saray <msaray.ms@outlook.com>
2021-06-06 14:00:06 -0700

    Converted State object to string & back to object.

--- js/main.js
+++ js/main.js
@@ -12,4 +12,13 @@ window.onload = function () {
     state.name = "Washington";
     state.city = "Tacoma";
     state.areaCode = 253;
+    var stateString = JSON.stringify(state);
+    var stateKey = "stateObject";
+    localStorage.setItem(stateKey, stateString);
+    var stateResult = localStorage.getItem(stateKey);
+    var currentState = JSON.parse(stateResult);
+    console.log(currentState);
+    displayInfo.innerHTML = currentState.city + ", " +
+        currentState.name + " " +
+        "(" + currentState.areaCode + ")";
 };
--- ts/main.ts
+++ ts/main.ts
@@ -24,5 +24,26 @@ window.onload = function(){
     let state = new State();
     state.name = "Washington";
     state.city = "Tacoma";
-    state.areaCode = 253;    
+    state.areaCode = 253; 
+    
+    /* convert all value in State object to a string using [JSON] to format. */
+    let stateString = JSON.stringify(state);
+
+    /* create global key variable for all data in State object. */
+    const stateKey = "stateObject"; // reusable create for intellisense.
+    
+    /* set State object: store State object in local storage. */
+    localStorage.setItem(stateKey, stateString); // (key, value)
+
+    /* get State object: read State object from local storage. */
+    let stateResult = localStorage.getItem(stateKey);
+
+    /* convert back to object. */
+    let currentState:State = JSON.parse(stateResult);
+    // display data in developer tool
+    console.log(currentState);
+    // display on web page.
+    displayInfo.innerHTML = currentState.city + ", " +
+                            currentState.name + " " +
+                            "(" + currentState.areaCode + ")";
 }
